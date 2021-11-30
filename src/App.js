84eedef2853bc08745780e80aa05d840a083f71d
Danish-mohammed <matifs.1992@gmail.com>
2021-11-30 17:46:41 +0530

    Added:To Display hello message with user input name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(){
     super()
     this.state= {
-      title: "Hello from Bridgelabz"
+      userName: ''
     }
   }
 
@@ -15,12 +15,23 @@ class App extends React.Component {
     console.log("save button is clicked!",event);
     window.open(this.url,"_blank");
   }
+
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    this.setState({userName: event.target.value})
+  }
+
   render(){
   return (
+    <>
     <div className="App">
-      <h1>{this.state.title}</h1>
+      <h1>Hello {this.state.userName} from Bridgelabz</h1>
       <img src={logo} onClick={this.handleClick} className="App-logo" alt="logo" />
     </div>
+    <div className="App">
+      <input onChange={this.onNameChange} />
+    </div>
+    </>
   );
   }
 }
