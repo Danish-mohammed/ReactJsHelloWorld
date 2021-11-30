3cda6a323f6dbd9c1c4e292259ea17453ee19a3c
Danish-mohammed <matifs.1992@gmail.com>
2021-11-30 17:35:35 +0530

    Added:To launch Bridgelabz site onclicking bridgelabz icon

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import logo from './bridgelabz_owler_20160302_231507_original.png';
 import './App.css';
 
 class App extends React.Component {
+  url='https://www.bridgelabz.com/';
   constructor(){
     super()
     this.state= {
       title: "Hello from Bridgelabz"
     }
   }
+
+  handleClick = (event) => {
+    console.log("save button is clicked!",event);
+    window.open(this.url,"_blank");
+  }
   render(){
   return (
     <div className="App">
       <h1>{this.state.title}</h1>
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} onClick={this.handleClick} className="App-logo" alt="logo" />
     </div>
   );
   }
