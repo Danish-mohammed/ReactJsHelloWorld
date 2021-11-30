0376c3dcf2882c713797456ad70d7bdad1f574eb
Danish-mohammed <matifs.1992@gmail.com>
2021-11-30 16:51:35 +0530

    Added:Display bridgelabz logo using property binding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './bridgelabz_owler_20160302_231507_original.png';
 import './App.css';
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
   return (
     <div className="App">
       <h1>{this.state.title}</h1>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
   }
